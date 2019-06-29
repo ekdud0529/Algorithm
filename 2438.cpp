e2df776a037e02f3df612378412bef62c99e8b48
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2019-06-30 01:37:41 +0900

    Add files via upload

---
+++ 2438.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int a, i, j;
+	scanf("%d", &a);
+	for(i=0;i<a;i++){
+		for(j=0;j<=i;j++){
+		printf("*");
+
+		}
+		printf("\n");
+	}
+	return 0;
+}
