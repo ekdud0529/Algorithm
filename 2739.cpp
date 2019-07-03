43c2066a79fae0713ab276c813d177f02380087a
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2019-07-03 14:52:02 +0900

    Add files via upload

---
+++ 2739.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char** argv) {
+	int n, i;
+	scanf("%d", &n);
+	for(i=1;i<10;i++){
+		printf("%d * %d = %d\n", n, i, n*i);
+	}
+	return 0;
+}
