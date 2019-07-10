a1f5f34389610d954dad797101b09c73cf2007a1
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2019-07-10 16:46:23 +0900

    Add files via upload

---
+++ 5338.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+printf("       _.-;;-._\n");
+printf("'-..-'|   ||   |\n");
+printf("'-..-'|_.-;;-._|\n");
+printf("'-..-'|   ||   |\n");
+printf("'-..-'|_.-''-._|");
+	return 0;
+}
