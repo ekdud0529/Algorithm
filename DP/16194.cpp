4e5f292fd855029a2f2a16b650c4b17d62f88ce3
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2020-11-18 14:27:15 +0900

    카드구매하기2

---
+++ DP/16194.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int cd[1001], p[1001];
+
+int main(int argc, char** argv) {
+	int n;
+	scanf("%d", &n);
+	
+	for(int i=1; i<=n; i++){
+		scanf("%d", &p[i]);
+	}
+	
+	for(int i=1; i<=n; i++){
+		cd[i] = 1000*10000;
+	}
+	
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=i; j++){
+			cd[i] = min(cd[i], cd[i-j]+p[j]);
+		}
+	}
+	
+	printf("%d", cd[n]);
+	return 0;
+}
