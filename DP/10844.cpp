ced4b0879f259fcc7bdbac1cb79b51aef572a23d
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2020-11-18 15:32:43 +0900

    쉬운계단수

--- DP/10844.cpp
+++ DP/10844.cpp
@@ -5,18 +5,16 @@ long long s[101][10];
 
 int main(int argc, char** argv) {
 	int n;
-	
+	scanf("%d", &n);
 	for(int i=1; i<10; i++) s[1][i] = 1;  
-	for(int i=2; i<=100; i++){
+	for(int i=2; i<=n; i++){
 		for(int j=0; j<=9; j++){
 			if(j-1>=0) s[i][j] += s[i-1][j-1];
 			if(j+1<10) s[i][j] += s[i-1][j+1];
 			s[i][j] %= 1000000000;
 		}
 	}
-	
 	long long sum=0;
-	scanf("%d", &n);
 	for(int j=0; j<=9; j++) sum += s[n][j];
 	printf("%d\n", sum%1000000000);
 	return 0;
