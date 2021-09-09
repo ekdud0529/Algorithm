c3d63584f65eef0f9871cb72e2fa498f177b4bb1
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2021-09-09 16:13:40 +0900

    N과 M (4)

---
+++ Baekjoon/Brute Force/15652.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[9], n, m;;
+
+void go(int ind, int start){
+	if(ind == m){
+		for(int i=0; i<ind; i++){
+			cout << a[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	
+	for(int j=start; j<=n; j++){
+		a[ind] = j;
+		go(ind+1, j);
+	}
+}
+
+int main(){
+	cin >> n >> m;
+	go(0, 1);
+	
+	return 0;
+}
