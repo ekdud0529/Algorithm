d190816e7e8687d35c2668859d8e6e472d997815
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2021-09-10 14:08:23 +0900

    N과 M (11)

---
+++ Baekjoon/Brute Force/15665.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, a[8], cnt;
+
+void go(int ind, int num[]){
+	if(ind == m){
+		for(int i=0; i<ind; i++){
+			cout << a[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	
+	for(int j=0; j<cnt; j++){
+		if(a[j] > 10000) continue;
+		a[ind] = num[j];
+		go(ind+1, num);
+	}
+}
+
+int main(){
+	cin >> n >> m;
+	int num[n];
+	for(int i=0; i<n; i++){
+		cin >> num[i];
+	}
+	for(int i=0; i<n; i++){
+		if(num[i] > 10000) continue;
+		cnt++;
+		for(int j=i+1; j<n; j++){
+			if(num[i] == num[j]){
+				num[j] = 10001;	
+			}
+		}
+	}
+	sort(num, num+n);
+	go(0, num);
+	return 0;
+}
