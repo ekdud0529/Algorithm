eb1d426428f507cbb10d8d8f6ba8004b11f34807
daykim <45174177+ekdud0529@users.noreply.github.com>
2021-11-02 15:53:02 +0900

    Create 두 정수 사이의 합.cpp

---
+++ Programmers/level1/두 정수 사이의 합.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    if(b < a)
+    {
+        int tmp = b;
+        b = a;
+        a = tmp;
+    }
+    for(int i=a; i<=b; i++)
+    {
+        answer += i;
+    }
+    return answer;
+}
