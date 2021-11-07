809d728a5bd7d0f5142f63eee9032617be9e1253
daykim <45174177+ekdud0529@users.noreply.github.com>
2021-11-07 22:32:01 +0900

    Create 콜라츠 추측.cpp

---
+++ Programmers/level1/콜라츠 추측.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long long n = num;
+    while(n != 1)
+    {
+        if(n % 2 == 0)
+            n /= 2;
+        else
+        {
+            n *= 3;
+            n++;
+        }
+        if(answer > 500)
+        {
+            answer = -1;
+            break;
+        }
+        answer++;
+    }
+    return answer;
+}
