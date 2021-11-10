dc6607d9092d439914675610df0d8c3a9cc6f6f8
daykim <45174177+ekdud0529@users.noreply.github.com>
2021-11-10 17:35:23 +0900

    Create x만큼 간격이 있는 n개의 숫자.cpp

---
+++ Programmers/level1/x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    long long add = x;
+    for(int i=0; i<n; i++)
+    {
+        answer.push_back(add);
+        add += x;
+    }
+    return answer;
+}
