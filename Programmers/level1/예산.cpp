2aa15c40fffa7e45b8afb744ee0d0b1657fcbae3
daykim <45174177+ekdud0529@users.noreply.github.com>
2021-11-16 17:02:01 +0900

    Create 예산.cpp

---
+++ Programmers/level1/예산.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = d.size();
+    long long sum = 0;
+    
+    sort(d.begin(), d.end());
+    
+    if(budget < d[0])
+        return 0;
+    
+    for(int i=0; i<d.size(); i++)
+    {
+        sum += d[i];
+    }
+    
+    if(sum <= budget)
+        return answer;
+    
+    else
+    {
+        for(int i=d.size()-1; i>=0; i--)
+        {
+            sum -= d[i];
+            answer--;
+            if(sum <= budget)
+            {
+                break;
+            }
+        }
+    }
+    return answer;
+}
