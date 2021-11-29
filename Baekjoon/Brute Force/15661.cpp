8b0d97ea64826b21aff8500b27495a5d13067f82
daykim <45174177+ekdud0529@users.noreply.github.com>
2021-11-29 19:10:17 +0900

    Create 15661.cpp

---
+++ Baekjoon/Brute Force/15661.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int		n;
+int		min_ability = 2000;
+int		member[20][20];
+bool	check[20];
+
+int get_ability(bool ch)
+{
+	int sum = 0;
+	for(int i=0; i<n; i++)
+	{
+		for(int k=i+1; k<n; k++)
+		{
+			if(check[i] == ch && check[k] == ch)
+			{
+				sum += (member[i][k] + member[k][i]);
+			}
+		}
+	}
+	return sum;
+}
+
+void get_min_ability(int cnt, int index, int mem_cnt)
+{
+	if(cnt == mem_cnt)
+	{
+		int sum = get_ability(true);
+		int other_sum = get_ability(false);
+		if(min_ability > abs(sum - other_sum))
+		{
+			min_ability = abs(sum - other_sum);
+		}
+		return ;
+	}
+
+	for(int i=index; i<n; i++)
+	{
+		if(check[i]) continue;
+		check[i] = true;
+		get_min_ability(cnt + 1, i, mem_cnt);
+		check[i] = false;
+	}
+}
+
+int main()
+{
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for(int i=0; i<n; i++)
+	{
+		for(int k=0; k<n; k++)
+		{
+			cin >> member[i][k];
+		}
+	}
+	for(int i=1; i<=n/2; i++)
+	{
+		get_min_ability(0, 0, i);
+	}
+	cout << min_ability;
+	return 0;
+}
