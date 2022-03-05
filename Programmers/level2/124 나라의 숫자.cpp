b8a2a09602e4ba6a8f4b3fc01db71376429e1d68
daykim <45174177+ekdud0529@users.noreply.github.com>
2022-03-05 17:06:08 +0900

    Create 124 나라의 숫자.cpp

---
+++ Programmers/level2/124 나라의 숫자.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    vector<int> num;
+    
+    while(n)
+    {
+        num.push_back(n % 3);
+        n /= 3;
+    }
+    for(int i=0; i<num.size(); i++)
+    {
+        if((num[i] == 0 || num[i] == -1) && i != num.size()-1)
+        {
+            num[i+1] -= 1;
+            num[i] += 3;
+        }
+    }
+    
+    for(int i=num.size()-1; i>=0; i--)
+    {
+        if(i == num.size()-1 && num[i] == 0) continue;
+        if(num[i] == 3) num[i] = 4;
+        answer += (num[i] + '0');
+    }
+    return answer;
+}
