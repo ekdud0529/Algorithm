c04771dab7b234602faf2eee82a76daa10035806
daykim <45174177+ekdud0529@users.noreply.github.com>
2022-09-22 16:38:10 +0900

    Update 피보나치 수.cpp

--- Programmers/level2/피보나치 수.cpp
+++ Programmers/level2/피보나치 수.cpp
@@ -16,3 +16,46 @@ int solution(int n) {
     answer = fibo[n] % 1234567;
     return answer;
 }
+
+
+
+
+
+---------------2---------------------
+    
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct fibo{
+    long long a, b, c, d;
+};
+
+fibo Cal(fibo a, fibo b)
+{
+    return {
+        (a.a * b.a + a.b * b.c) % 1234567,
+        (a.a * b.b + a.b * b.d) % 1234567,
+        (a.c * b.a + a.d * b.c) % 1234567,
+        (a.c * b.b + a.d * b.d) % 1234567
+    };
+}
+
+int solution(int n) {
+    int answer = 0;
+    fibo cal = {1, 1, 1, 0};
+    fibo ans = {1, 0, 1, 0};
+    
+    if(n <= 2)
+        return (1);
+    n -= 2;
+    for(; n > 0; n = n >> 1)
+    {
+        if(n & 1)
+            ans = Cal(cal, ans);
+        cal = Cal(cal, cal);
+    }
+    answer = (ans.a) % 1234567;
+    return answer;
+}
