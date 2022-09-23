6f6b480cddf7756a145b2b1b7dacc409f2f303bb
daykim <45174177+ekdud0529@users.noreply.github.com>
2022-09-23 16:09:17 +0900

    Create 점프와 순간 이동.cpp

---
+++ Programmers/level2/점프와 순간 이동.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+    
+    while(n)
+    {
+        if((n & 1) == 1)
+        {
+            ans++;
+            n--;
+        }
+        n /= 2;
+    }
+    return ans;
+}
