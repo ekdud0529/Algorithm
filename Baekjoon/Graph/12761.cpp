73e3b98e2558a7870f12dd2976fed7e470e246dd
daykim <45174177+ekdud0529@users.noreply.github.com>
2022-09-29 15:55:54 +0900

    돌다리

--- Baekjoon/Graph/12761.cpp
+++ Baekjoon/Graph/12761.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+#include <utility>
 using namespace std;
 
 bool visited[100001];
@@ -7,43 +9,40 @@ int main(int argc, char** argv) {
 	int a, b, n, m;
 	int min_move = 1000000;
 	cin >> a >> b >> n >> m;
-	
-	int move[3] = {1, a, b};
-	
+
+	int move[6] = { 1, a, b, -1, -a, -b};
+
 	queue<pair<int, int> > pos;
-	pos.push({n, 0});
-	
-	while(!pos.empty())
+	visited[n] = true;
+	pos.push({ n, 0 });
+
+	while (!pos.empty())
 	{
 		int dong = pos.front().first;
 		int	cnt = pos.front().second;
 		visited[dong] = true;
 		pos.pop();
-		
-		cout << dong << " " << cnt << "\n";
-		if(dong == m)
+
+		if (dong == m)
 		{
 			cout << cnt;
-			break; 
+			break;
 		}
-		for(int i=0; i<3; i++)
+		for (int i = 0; i < 6; i++)
 		{
-			if(!visited[dong+move[i]] && (dong + move[i] <= 100000))
-			{
-				visited[dong+move[i]] = true;
-				pos.push({dong+move[i], cnt+1});
-			}
-			if(!visited[dong-move[i]] && (0 <= dong - move[i]))
-			{
-				visited[dong-move[i]] = true;
-				pos.push({dong-move[i], cnt+1});
-			}
-			if(move[i] == 1) continue;
-			if(!visited[dong*move[i]] && (dong * move[i] <= 100000))
-			{
-				visited[dong*move[i]] = true;
-				pos.push({dong*move[i], cnt+1});	
-			}
+			int mv = dong + move[i];
+			if (mv < 0 || mv > 100000) continue;
+			if (visited[mv]) continue;
+			visited[mv] = true;
+			pos.push({ mv, cnt + 1 });
+		}
+		for (int i = 1; i <= 2; i++)
+		{
+			int mv = dong * move[i];
+			if (mv < 0 || mv > 100000) continue;
+			if (visited[mv]) continue;
+			visited[mv] = true;
+			pos.push({ mv, cnt + 1 });
 		}
 	}
 	return 0;
