23f5759a1acd69c3f8b299db7a21f512230505bc
daykim <45174177+ekdud0529@users.noreply.github.com>
2022-10-23 17:16:45 +0900

    Create 스킬트리.cpp

---
+++ Programmers/level2/스킬트리.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+bool check[26];
+
+int solution(string skill, vector<string> skill_trees) {
+    int answer = 0;
+    vector<string> tree;
+    
+    for(char c : skill)
+    {
+        check[c - 'A'] = true;
+    }
+    for(int i=0; i<skill_trees.size(); i++)
+    {
+        string str = "";
+        for(int j=0; j<skill_trees[i].size(); j++)
+        {
+            if(check[skill_trees[i][j] - 'A'])
+            {
+               str += skill_trees[i][j]; 
+            }
+        }
+        tree.push_back(str);
+    }
+    for(string sk : tree)
+    {
+        bool flag = true;
+        for(int i=0; i<sk.size(); i++)
+        {
+            if(sk[i] != skill[i])
+            {
+                flag = false;
+                break;
+            }
+        }
+        if(flag) answer++;
+    }
+    return answer;
+}
